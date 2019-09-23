1d0b2e1538bd611419e1f0a1d63a6395a36234db
Rhet Turnbull <rturnbull@gmail.com>
2019-09-22 21:54:20 -0700

    removed license from script file since it's in LICENSE file

--- photosmeta.py
+++ photosmeta.py
@@ -3,7 +3,7 @@
 # photosmeta
 # Copyright (c) 2018 Rhet Turnbull <rturnbull+git@gmail.com>
 #
-# Version 0.1
+# Version 0.15
 #
 # Tested with Mac OS 10.13.6 and Photos Version 3.0 (3291.13.210)
 #
@@ -38,26 +38,6 @@
 
 # NOTE: This is my very first python project. Using this script might
 # completely destroy your Photos library.  You have been warned! :-)
-
-# Permission is hereby granted, free of charge, to any person
-# obtaining a copy of this software and associated documentation files
-# (the "Software"), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge,
-# publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 # ## THINGS TODO ###
 # todo: progress bar for photos to process
